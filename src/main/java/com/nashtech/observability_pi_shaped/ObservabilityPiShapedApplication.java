3ae903690ec4f9b461d11f9b82e72bfb256b308c
Nadra Ibrahim <nadra.ibrahim@nashtechglobal.com>
2026-02-04 19:10:23 +0530

    Setup the basic project

---
+++ src/main/java/com/nashtech/observability_pi_shaped/ObservabilityPiShapedApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.observability_pi_shaped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityPiShapedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityPiShapedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nashtech/observability_pi_shaped/ObservabilityPiShapedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.observability_pi_shaped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityPiShapedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
